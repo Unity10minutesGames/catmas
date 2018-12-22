94d6796a568fdbb7ca070920a98a2bd60c1f9b0a
smeerws <hello@smeerws.at>
2018-12-22 20:18:01 +0100

    added x axis movement with keyboard

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private const string AXISHORIZONTAL = "Horizontal";
+    private float xMin, xMax;
+
+    public float moveSpeed = 5.0f;
+    public float padding = 0.5f;
+
+    
+
+    private void SetupMoveBounderies()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x - padding;
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        transform.position = new Vector2(newPosX, transform.position.y);
+    }
+
+	// Use this for initializations
+	void Start ()
+    {
+        SetupMoveBounderies();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();	
+	}
+}
