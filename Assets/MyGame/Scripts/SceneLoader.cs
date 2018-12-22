f2e9ce4651dc5c124faffaebf82d35f260577339
smeerws <hello@smeerws.at>
2018-12-22 15:10:30 +0100

    basic scene management

--- Assets/MyGame/Scripts/SceneLoader.cs
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    enum Scenes { Welcome, MainGame, GameOver};
+
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Welcome);
+    }
+
+    public void LoadMainGameScene()
+    {
+        SceneManager.LoadScene((int)Scenes.MainGame);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOver);
+    }
 }
