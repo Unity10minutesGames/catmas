639d5d84252ad02347b7a89cafe15a8fe0151d2b
smeerws <hello@smeerws.at>
2019-01-19 17:40:18 +0100

    added animations

---
+++ Assets/MyGame/Scripts/CatController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+    public Sprite catIdleBehind;
+    private Sprite catIdleAhead;
+
+    private const string STATESCRATCH = "scratch";
+    private Player player;
+    
+
+    private Animator anim;
+
+
+
+    private void Turn()
+    {
+        if (player.CharacterEyesPlayer)
+        {
+            GetComponent<SpriteRenderer>().sprite = catIdleBehind;
+            player.CharacterEyesPlayer = false;
+            GetComponent<Animator>().SetBool("eyesPlayer", false);
+            return;
+        }
+
+        GetComponent<SpriteRenderer>().sprite = catIdleAhead;
+        GetComponent<Animator>().SetBool("eyesPlayer", true);
+        player.CharacterEyesPlayer = true;
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        catIdleAhead = GetComponent<SpriteRenderer>().sprite;
+        player = GetComponentInParent<Player>();
+        GetComponent<Animator>().SetBool("eyesPlayer", true);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("in Turn");
+            Turn();
+        }
+    }
+}
--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private const string AXISHORIZONTAL = "Horizontal";
     private const string AXISVERTICAL = "Vertical";
-    private const string STATESCRATCH = "scratch";
     private const string PRESENTBEHIND = "PresentBehind";
     private const string PRESENTAHEAD = "PresentAhead";
+    private bool characterEyesPlayer = true;
+
     private float xMin, xMax;
 
-    private Animator anim;
-    int catscratch = Animator.StringToHash(STATESCRATCH);
-    int catidle = Animator.StringToHash("catidle");
     public float moveSpeed = 5.0f;
     public float padding = 0.5f;
-    public Sprite catIdleBehind;
-    private Sprite catIdleAhead;
-    
-    private bool characterEyesPlayer = true;
     int currentpos = 0;
-    
+
+    public bool CharacterEyesPlayer
+    {
+        get
+        {
+            return characterEyesPlayer;
+        }
+
+        set
+        {
+            characterEyesPlayer = value;
+        }
+    }
 
     private void SetupMoveBounderies()
     {
@@ -37,58 +43,29 @@ public class Player : MonoBehaviour {
         transform.position = new Vector2(newPosX, transform.position.y);
     }
 
-    private void Turn()
-    {
-        if (characterEyesPlayer)
-        {
-            GetComponent<SpriteRenderer>().sprite = catIdleBehind;
-            characterEyesPlayer = false;
-            return;
-        }
-
-        GetComponent<SpriteRenderer>().sprite = catIdleAhead;
-        characterEyesPlayer = true;
-
-    }
-
 	// Use this for initializations
 	void Start ()
     {
-        catIdleAhead = GetComponent<SpriteRenderer>().sprite;
         SetupMoveBounderies();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         //Input.HorizontalAxix
         Move();
-
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Turn();
-        }
-        //{
-        //    Debug.Log("Arrow down pressed");
-        //    anim.SetTrigger(catscratch);
-        //}
-
-        //if (Input.GetAxis(AXISVERTICAL))
-        //{
-
-        //}
-
 	}
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        //Debug.Log("in on Trigger Stay");
         //if(collision.name == "present2Behind")
-        if(collision.tag == PRESENTBEHIND && !characterEyesPlayer)
+        if(collision.tag == PRESENTBEHIND && !CharacterEyesPlayer)
         {
             HandlePresent(collision);
         }
 
-        if (collision.tag == PRESENTAHEAD && characterEyesPlayer)
+        if (collision.tag == PRESENTAHEAD && CharacterEyesPlayer)
         {
             HandlePresent(collision);
         }
@@ -96,8 +73,11 @@ public class Player : MonoBehaviour {
         //collision.gameObject.GetComponent<Present>().destroyPresent();
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("OntriggerExit");
+        GetComponentInChildren<Animator>().SetBool("excited", false);
+        GetComponentInChildren<Animator>().SetBool("scratch", false);
         StopAllCoroutines();
     }
 
@@ -105,22 +85,31 @@ public class Player : MonoBehaviour {
     {
         if (collision.gameObject.GetComponent<Present>().isRightPresent(currentpos))
         {
-            currentpos++;
-            currentpos = (int)Mathf.Clamp(currentpos, 0, 3);
-            Vector3 tmpPos = gameObject.GetComponent<Transform>().position;
-            GetComponent<Animator>().SetBool("excited", true);
-            GetComponent<Animator>().SetBool("scratch", true);
+            
+            
+            //currentpos = (int)Mathf.Clamp(currentpos, 0, 3);
+            //Vector3 tmpPos = gameObject.GetComponent<Transform>().position;
+            //GetComponent<Animator>().SetBool("scratch", true);
 
-            StartCoroutine(WaitForScratch(collision.name));
+            if (currentpos < 4)
+            {
+                StartCoroutine(WaitForScratch(collision));
+            }
             //collision.gameObject.GetComponent<Present>().destroyPresent();
             Debug.Log("in Handle Present");
         }
     }
 
-    private IEnumerator WaitForScratch(string colname)
+    private IEnumerator WaitForScratch(Collider2D collision)
     {
+        Debug.Log("charater ayes plyer "+CharacterEyesPlayer);
+        GetComponentInChildren<Animator>().SetBool("eyesWall", CharacterEyesPlayer);
+        GetComponentInChildren<Animator>().SetBool("excited", true);
+        yield return new WaitForSeconds(2);
+        GetComponentInChildren<Animator>().SetBool("scratch", true);
+        GetComponentInChildren<Animator>().SetBool("eyesWall", CharacterEyesPlayer);
         yield return new WaitForSeconds(3);
-        Debug.Log("start anim scratch in penent: " + colname);
-
+        Destroy(collision.gameObject);
+        currentpos++;
     }
 }
